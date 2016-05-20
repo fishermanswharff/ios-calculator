85abc3f31232fdb72bd5a632a295ee0e9f8bc713
Jason Wharff <fishermanswharff@mac.com>
2016-05-20 19:41:23 -0400

    initial commit

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jason Wharff on 5/20/16.
+//  Copyright © 2016 fishermanswharff. All rights reserved.
+//
+
+// uikit is a module
+// has all the user interface things, you're almost always going to import uikit
+import UIKit
+
+// ViewController inherits from UIViewController
+// All controllers in Swift apps must inherit from UIViewController
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
