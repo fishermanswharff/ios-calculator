5e962bb3e75152dc33efef27148085581931c492
Jason Wharff <fishermanswharff@mac.com>
2016-05-21 19:06:55 -0400

    mvc pattern, calculator app built while following cs193p lecture 2

---
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,72 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Jason Wharff on 5/21/16.
+//  Copyright © 2016 fishermanswharff. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+  private var accumulator = 0.0
+
+  func setOperand(operand: Double) {
+    accumulator = operand
+  }
+
+  private var operations: Dictionary<String,Operation> = [
+    "π": Operation.Constant(M_PI),
+    "e": Operation.Constant(M_E),
+    "√": Operation.UnaryOperation(sqrt),
+    "cos": Operation.UnaryOperation(cos),
+    "×": Operation.BinaryOperation({ $0 * $1 }),
+    "÷": Operation.BinaryOperation({ $0 / $1 }),
+    "+": Operation.BinaryOperation({ $0 + $1 }),
+    "−": Operation.BinaryOperation({ $0 - $1 }),
+    "=": Operation.Equals
+  ]
+
+  private enum Operation {
+    case Constant(Double)
+    case UnaryOperation((Double) -> Double)
+    case BinaryOperation((Double, Double) -> Double)
+    case Equals
+  }
+
+  func performOperation(symbol: String) {
+    if let operation = operations[symbol] {
+      switch operation {
+      case .Constant(let value):
+        accumulator = value
+      case .UnaryOperation(let function):
+        accumulator = function(accumulator)
+      case .BinaryOperation(let function):
+        executePendingBinaryOperation()
+        pending = PendingBinaryOperationInformation(binaryFunction: function, firstOperand: accumulator)
+      case .Equals:
+        executePendingBinaryOperation()
+      }
+    }
+  }
+
+  private func executePendingBinaryOperation(){
+    if pending != nil {
+      accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+      pending = nil
+    }
+  }
+
+  private var pending: PendingBinaryOperationInformation?
+
+  private struct PendingBinaryOperationInformation {
+    var binaryFunction: (Double, Double) -> Double
+    var firstOperand: Double
+  }
+
+  var result: Double {
+    get {
+      return accumulator
+    }
+  }
+}
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -14,13 +14,57 @@ import UIKit
 // All controllers in Swift apps must inherit from UIViewController
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  //
+  @IBOutlet private weak var display: UILabel!
+
+  private var userIsInTheMiddleOfTyping = false
+  // func => method on a class
+  // you could have global funcs as well, outside the definition of the class.
+  // you can pass other arguments, argumentName: keyWord
+  @IBAction private func touchDigit(sender: UIButton) {
+    // to call methods on the class, use the 'self' syntax
+    //  self.touchDigit(someButton, otherArgument: 5)
+
+    let digit = sender.currentTitle!
+
+    // print("touched \(digit)")
+    // Optionals: in swift, there is a type called 'Optional'
+    // this type can only have two values: notSet (nil), and set
+    // if it's in a set state, it will have an associated value.
+    // swift will infer the type of the digit, in this case, currentTitle is a String? (Optional) so digit is inferred to be a String?
+    // You get the associated value out of an optional by using !
+
+    if userIsInTheMiddleOfTyping {
+      let textCurrentlyInDisplay = display.text!
+      display.text = textCurrentlyInDisplay + digit
+    } else {
+      display.text = digit
+    }
+    userIsInTheMiddleOfTyping = true
+
+    // you can set optionals to an associated value or nil
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  private var displayValue: Double {
+    get {
+      return Double(display.text!)!
+    }
+    set {
+      display.text = String(newValue)
+    }
+  }
+
+  private var brain = CalculatorBrain()
+
+  @IBAction private func performOperation(sender: UIButton) {
+    if userIsInTheMiddleOfTyping {
+      brain.setOperand(displayValue)
+      userIsInTheMiddleOfTyping = false
+    }
+    if let mathematicalSymbol = sender.currentTitle {
+      brain.performOperation(mathematicalSymbol)
+    }
+
+    displayValue = brain.result
   }
 }
