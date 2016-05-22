727379cc3dde6ee9b042055c073e0bbb5148d772
Jason Wharff <fishermanswharff@mac.com>
2016-05-22 09:18:32 -0400

    adding all clear button, logic for legal floating points

--- Calculator/Calculator/CalculatorBrain.swift
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -15,6 +15,29 @@ class CalculatorBrain {
     accumulator = operand
   }
 
+  func performOperation(symbol: String) {
+    if let operation = operations[symbol] {
+      switch operation {
+      case .Constant(let value):
+        accumulator = value
+      case .UnaryOperation(let function):
+        accumulator = function(accumulator)
+      case .BinaryOperation(let function):
+        executePendingBinaryOperation()
+        pending = PendingBinaryOperationInformation(binaryFunction: function, firstOperand: accumulator)
+      case .Equals:
+        executePendingBinaryOperation()
+      }
+    }
+  }
+
+  func clearAll() {
+    pending = nil
+    accumulator = 0.0
+  }
+
+  private var pending: PendingBinaryOperationInformation?
+
   private var operations: Dictionary<String,Operation> = [
     "π": Operation.Constant(M_PI),
     "e": Operation.Constant(M_E),
@@ -34,20 +57,9 @@ class CalculatorBrain {
     case Equals
   }
 
-  func performOperation(symbol: String) {
-    if let operation = operations[symbol] {
-      switch operation {
-      case .Constant(let value):
-        accumulator = value
-      case .UnaryOperation(let function):
-        accumulator = function(accumulator)
-      case .BinaryOperation(let function):
-        executePendingBinaryOperation()
-        pending = PendingBinaryOperationInformation(binaryFunction: function, firstOperand: accumulator)
-      case .Equals:
-        executePendingBinaryOperation()
-      }
-    }
+  private struct PendingBinaryOperationInformation {
+    var binaryFunction: (Double, Double) -> Double
+    var firstOperand: Double
   }
 
   private func executePendingBinaryOperation(){
@@ -57,12 +69,6 @@ class CalculatorBrain {
     }
   }
 
-  private var pending: PendingBinaryOperationInformation?
-
-  private struct PendingBinaryOperationInformation {
-    var binaryFunction: (Double, Double) -> Double
-    var firstOperand: Double
-  }
 
   var result: Double {
     get {
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -6,43 +6,32 @@
 //  Copyright © 2016 fishermanswharff. All rights reserved.
 //
 
-// uikit is a module
-// has all the user interface things, you're almost always going to import uikit
 import UIKit
 
-// ViewController inherits from UIViewController
-// All controllers in Swift apps must inherit from UIViewController
 class ViewController: UIViewController {
 
-  //
   @IBOutlet private weak var display: UILabel!
 
   private var userIsInTheMiddleOfTyping = false
-  // func => method on a class
-  // you could have global funcs as well, outside the definition of the class.
-  // you can pass other arguments, argumentName: keyWord
+
   @IBAction private func touchDigit(sender: UIButton) {
-    // to call methods on the class, use the 'self' syntax
-    //  self.touchDigit(someButton, otherArgument: 5)
 
     let digit = sender.currentTitle!
 
-    // print("touched \(digit)")
-    // Optionals: in swift, there is a type called 'Optional'
-    // this type can only have two values: notSet (nil), and set
-    // if it's in a set state, it will have an associated value.
-    // swift will infer the type of the digit, in this case, currentTitle is a String? (Optional) so digit is inferred to be a String?
-    // You get the associated value out of an optional by using !
-
     if userIsInTheMiddleOfTyping {
       let textCurrentlyInDisplay = display.text!
-      display.text = textCurrentlyInDisplay + digit
+
+      if digit == "." {
+        if textCurrentlyInDisplay.rangeOfString(".") == nil {
+          display.text = textCurrentlyInDisplay + "."
+        }
+      } else {
+        display.text = textCurrentlyInDisplay + digit
+      }
     } else {
       display.text = digit
     }
     userIsInTheMiddleOfTyping = true
-
-    // you can set optionals to an associated value or nil
   }
 
   private var displayValue: Double {
@@ -66,5 +55,10 @@ class ViewController: UIViewController {
     }
 
     displayValue = brain.result
+  }
+
+  @IBAction private func allClear(sender: UIButton) {
+    display.text = "0"
+    brain.clearAll()
   }
 }
